a133bb50ed889a6f2ed65f374aef70f197bfbc2a
mathis <mathis.gemke@gmail.com>
2019-09-20 22:47:25 +0200

    Wlan angefangen

--- sketch_sep20a/sketch_sep20a.ino
+++ sketch_sep20a/sketch_sep20a.ino
@@ -3,7 +3,8 @@ const int taster =  5; //pin D1
 const int led_rot= 4; //pin D2
 const int led_gruen = 14; //pin D5 
 const int led_blau = 15; //pin D8
-int tasterstatus = 0; 
+int tasterstatus = 0;
+int zustand = 0; 
 
 void setup() {
   pinMode(taster, INPUT_PULLUP);
@@ -12,6 +13,8 @@ void setup() {
   pinMode(led_rot, OUTPUT);
   pinMode(led_gruen, OUTPUT);
   pinMode(led_blau, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("SSID", "PASSWORD");
 }
 void loop () {
   digitalWrite(led_blau, LOW);
@@ -19,12 +22,16 @@ void loop () {
   if (tasterstatus == HIGH) {
     Serial.println("Kein Fahrrad da");
     digitalWrite(led_rot, LOW);
-    digitalWrite(led_gruen, HIGH);
+    digitalWrite(led_gruen, HIGH);    
   }
   else {
     Serial.println("Fahrrad drin");
     digitalWrite(led_gruen, LOW);
     digitalWrite(led_rot, HIGH);
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      WiFiClient client; //hier das Paket verschicken!
+      Serial.println("Wlan aktiv");
+    }
   }
   delay(1000);
 }
